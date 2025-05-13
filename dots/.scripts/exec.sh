e26070bc7d62b4f3c88030f29a0854c7153c3008
369wn <369wn@pm.me>
2025-05-12 21:37:09 -0300

    fixing dmenu scripts

--- dots/.scripts/exec.sh
+++ dots/.scripts/exec.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Exibe o prompt do dmenu e captura o comando digitado
-cmd=$(echo "" | dmenu -p "Command:")
+# Exibe o prompt do dmenu com as novas configurações e captura o comando digitado
+cmd=$(echo "" | dmenu -x 550 -y 300 -z 250 -l 5 -fn "FiraCode Nerd Font:size=10" -nb "#111111" -nf "#bbbbbb" -sb "#afafaf" -sf "#111111" -p "Command:")
 
 # Verifica se o usuário digitou algo e executa o comando
 if [ -n "$cmd" ]; then
     eval "$cmd"
 fi
-
--- dots/.scripts/powermenu.sh
+++ dots/.scripts/powermenu.sh
@@ -2,7 +2,7 @@
 
 function powermenu {
   options="Cancel\nShutdown\nRestart\nLock"
-  selected=$(echo -e "$options" | dmenu)
+  selected=$(echo -e "$options" | dmenu -x 550 -y 300 -z 250 -l 5 -fn "FiraCode Nerd Font:size=10" -nb "#111111" -nf "#bbbbbb" -sb "#afafaf" -sf "#111111")
   if [[ $selected = "Shutdown" ]]; then
     loginctl poweroff
   elif [[ $selected = "Restart" ]]; then
